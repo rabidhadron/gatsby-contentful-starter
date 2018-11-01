ffe55e538dee9da48e90253157f91ac847e54772
rabidhadron <andrew.jones@sds.ac>
2018-11-01 08:54:02 +0000

    First commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { StaticQuery, graphql } from "gatsby";
+
+const HomePage = () => (
+    <StaticQuery
+        query={graphql`
+            query HomePage {
+                contentfulHomePage {
+                    title
+                    date
+                    content {
+                        childMarkdownRemark {
+                            html
+                        }
+                    }
+                    image {
+                        file {
+                            url
+                        }
+                    }
+                }
+            }
+        `}
+        render={({
+            contentfulHomePage: {
+                title,
+                date,
+                content: {
+                    childMarkdownRemark: { html }
+                },
+                image: {
+                    file: { url }
+                }
+            }
+        }) => (
+            <>
+                <h1>{title}</h1>
+                <small>Created on {date}</small>
+                <img src={url} alt=""/>
+                <div dangerouslySetInnerHTML={{ __html: html }} />
+            </>
+        )}
+    />
+);
+
+export default HomePage;
